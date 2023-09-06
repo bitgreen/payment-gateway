9a18a29e824130d2290b3a99511a672eeb52f14c
Samuele Landi <samuele.landi@kryptotel.net>
2023-09-06 05:18:46 +0000

    added the creation of a set of orders

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ async function mainloop(){
        password: 'testpwd',
      });
      //connect
-    await client.connect();
+    //await client.connect();
     console.log("Connection to database completed...");
     // connect to testnet
     // connect to the substrate Node:
@@ -30,7 +30,7 @@ async function mainloop(){
     // for testing on testnet
     const MNEMONIC="pear art cup mirror skate state engine repair state crouch reopen main";
     const keyring = new Keyring();
-    const keypair=keyring.createFromUri(MNEMONIC);
+    const keypair=keyring.createFromUri(MNEMONIC,{ type: 'sr25519'});
     console.log("Created testing account:",keypair.address);
     // check balance of testing account
     const account = await api.query.system.account(keypair.address);
@@ -44,8 +44,117 @@ async function mainloop(){
       return;
     }
     console.log("Testing account has funds..(OK)");
-    await api.disconnect();
-    await client.end();
+    /*
+      we have a set of projects on testnet to be used for the unit tests:
+      projectId : 35
+      assetId : 38
+      sellOrderId : 54
+      Name : unitTest1
+      Credits : 1m
+      Originator : 5H3R2XduBHH5uXHaJhYQeFqfFzMJct67xTZq9G7QWP5tNxX4
+
+      projectId : 36
+      assetId : 37
+      sellOrderId : 53
+      Name : unitTest2
+      Credits : 1m
+      Originator : 5H3R2XduBHH5uXHaJhYQeFqfFzMJct67xTZq9G7QWP5tNxX4
+
+      projectId : 37
+      assetId : 36
+      sellOrderId : 55
+      Name : unitTest3
+      Credits : 1m
+      Originator : 5H3R2XduBHH5uXHaJhYQeFqfFzMJct67xTZq9G7QWP5tNxX4
+
+
+      projectId : 38
+      assetId : 35
+      sellOrderId : 56
+      Name : unitTest4
+      Credits : 1m
+      Originator : 5Eex6tNv5eme8GSXK9FcPdgk2e88KS3YqVTXYPdm8LAmX23H
+
+      projectId : 39
+      assetId : 34
+      sellOrderId : 57
+      Name : unitTest5
+      Credits : 1m
+      Originator : 5Eex6tNv5eme8GSXK9FcPdgk2e88KS3YqVTXYPdm8LAmX23H
+    */
+    // create a cart of orders in an object
+    let cart=[
+      {projectId : 35,assetId : 38,sellOrderId:54,qnt: 2},
+      {projectId : 36,assetId : 37,sellOrderId:53,qnt: 1},
+      {projectId : 37,assetId : 38,sellOrderId: 55,qnt: 3},
+      {projectId : 38,assetId : 35,sellOrderId: 56,qnt: 5},
+      {projectId : 39,assetId : 34,sellOrderId: 57,qnt: 4}
+    ]
+    //submit the purchase order on dex
+    const nonce = await api.rpc.system.accountNextIndex(keypair.address);
+    //let hash=await api.tx.dex.createBuyOrder(54,38,1,10).signAndSend(keypair,{ nonce });
+    //console.log("hashtx",hash.toHuman());
+    
+    const txs = [];
+    for (let i=0;i<cart.length;i++){
+      txs.push(api.tx.dex.createBuyOrder(cart[i].sellOrderId,cart[i].assetId,cart[i].qnt,10));
+    }
+    console.log(txs);
+    // construct the batch and send the transactions
+    const r = await api.tx.utility.batch(txs)
+    .signAndSend(keypair, { nonce }, ({ status, events = [], dispatchError }) => {
+      if(dispatchError) {
+          // for module errors, we have the section indexed, lookup
+          const decoded = api.registry.findMetaError(dispatchError.asModule)
+          const { docs, method, section } = decoded
+
+          if(dispatchError.isModule) {
+              response = {
+                  success: false,
+                  status: 'failed',
+                  error: section + '.' + method + ' ' + docs.join(' '),
+                  data: {
+                      section,
+                      method
+                  }
+              }
+          } else {
+              // Other, CannotLookup, BadOrigin, no extra info
+              response = {
+                  success: false,
+                  status: 'failed',
+                  error: dispatchError.toString()
+              }
+          }
+          // returns answer with error
+          console.log(response);
+          return(repsponse);
+      }
+      if(status.isInBlock) {
+        // returns success
+        const answer={
+            success: true,
+            data: {
+                block_hash: status.asInBlock.toHex()
+            }
+        };
+        console.log(answer);
+        return(answer);
+      }
+    }).catch(err => {
+        // returns errors in other case
+        const answer={
+            success: false,
+            status: 'failed',
+            error: err.message
+        };
+        console.log(answer);
+        return(answer);
+    });
+    
+    console.log("Result:",r);
+    //await api.disconnect();
+    //await client.end();
     return;   
        
     // book a carbon credit purchase on dex
